90709c5eb2c0526c0725c0ee89dde3295f9f1075
abyshake-anand <abishakeanand+git@gmail.com>
2025-01-07 06:49:49 +0530

    initial setup

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run generate
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPOSITORY>
+git push -f git@github.com:Peakmails/peakmails.github.io.git main:gh-pages
+
+cd -
