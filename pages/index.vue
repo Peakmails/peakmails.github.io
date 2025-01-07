a1c3caa3bb1173f55eff442a463b048c62ff5f7c
abyshake-anand <abishakeanand+git@gmail.com>
2025-01-07 07:10:51 +0530

    initial setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="">Hello</div>
+    <div class="bg-gray-100 h-screen w-screen flex items-center justify-center text-center text-4xl font-medium text-gray-700">peakmails.github.io</div>
 </template>
 
 <script setup lang="ts">
